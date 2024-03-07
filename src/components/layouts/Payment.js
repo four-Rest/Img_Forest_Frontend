34706af2f240b0a192b24c0a06965e61bb3c732c
tallguy188 <cksghd4220@naver.com>
2024-03-07 16:57:15 +0900

    feat: 테스트 결제 추가
    테스트 결제 추가

---
+++ src/components/layouts/Payment.js
@@ -0,0 +1,62 @@
+import React, {useEffect} from 'react';
+import axios from 'axios';
+
+
+const Payment = () => {
+    useEffect(() => {
+
+        const jquery = document.createElement("script");
+        jquery.src = "http://code.jquery.com/jquery-1.12.4.min.js";
+        const iamport = document.createElement("script");
+        iamport.src = "http://cdn.iamport.kr/js/iamport.payment-1.1.7.js";
+        document.head.appendChild(jquery);
+        document.head.appendChild(iamport);
+
+        return () => {
+            document.head.removeChild(jquery);
+            document.head.removeChild(iamport);
+        };
+
+    }, []);
+
+
+
+    // 포트원 개발자 사이트 v1버전. 
+    const requestPay = () => {
+        const { IMP } = window;
+        IMP.init('imp54562367');
+
+        IMP.request_pay({
+            pg: 'kakaopay.TC0ONETIME',
+            pay_method: 'card',
+            merchant_uid: new Date().getTime(),
+            name: '테스트 상품',
+            amount: 1000,
+            buyer_name: '찬홍',
+            buyer_tel:'010-9896-4220',
+
+        }, async(rsp) => {
+        try{
+            const{ data }  = await axios.post('http://localhost:8090/api/payment/validation/' + rsp.imp_uid);
+
+                    
+            if(rsp.paid_amount === data.response.amount) {
+                alert('결제 성공');
+            }else {
+                alert('결제 실패');
+                }
+        }catch(error) {
+            console.error('Error while verifying payment' , error);
+            alert('결제 실패');
+            }
+        });
+    };
+
+    return (
+        <div>
+            <button onClick = {requestPay}>결제하기</button>
+        </div>
+    );
+};
+
+export default Payment;

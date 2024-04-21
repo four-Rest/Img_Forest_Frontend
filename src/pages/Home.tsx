f498ff46cb46306f5bfbe54f6053f0ab01f1d9ef
thseo1758 <thseo1758@gmail.com>
2024-04-22 01:44:25 +0900

    feat:홈 무한스크롤 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,85 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Masonry from 'react-masonry-css';
+import Paging from '../delete/paging';
+import { useQuery } from '@tanstack/react-query';
+
+type ArticleData = {
+  id: string;
+  imgFileName: string;
+
+  imgFilePath: string;
+  likes: number;
+  paid: boolean;
+};
 
 const Home = () => {
-
+  const navigate = useNavigate();
+  const location = useLocation();
+  const target = useRef<HTMLDivElement>(null);
+  const [articleData, setArticleData] = useState<ArticleData[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [startIndex, setStartIndex] = useState(0); // 표시할 이미지의 시작 인덱스
+  const [endIndex, setEndIndex] = useState(10); // 표시할 이미지의 마지막 인덱스
+  const apiBaseUrl = process.env.REACT_APP_CORE_IMAGE_BASE_URL;
   const apiUrl = process.env.REACT_APP_CORE_API_BASE_URL;
-  useEffect(()=>{
-      const login = async () => {
-          const response = await fetch(`${apiUrl}/api/member/login`, {
-              headers: {
-                "Content-Type": "application/json",
-              },
-              method: "POST",
-              credentials: "include",
-              body: JSON.stringify({
-                username: "test666",
-                password: "test666",
-              }),
-            }).catch(err => {
-              return null;
-            })
-            if (response != null) {
-              const res = await response.json(); // 응답이 성공적인 경우에만 JSON 파싱
-              localStorage.setItem("username", res.data.username);
-              localStorage.setItem("nickname", res.data.nickname);
-              localStorage.setItem('isLogin', 'true');
-              console.log("로그인")
+
+  const { isPending, error, data, refetch } = useQuery({
+    queryKey: ['imageData'],
+    queryFn: async () => {
+      const res = await fetch(`${apiUrl}/api/article`); // API에서 이미지 데이터를 가져옴
+      const data = await res.json(); // 응답 데이터를 JSON 형식으로 변환
+
+      //데이터의 길이를 받아와 준다.?여기선안해도될듯
+      setArticleData(data.data.content); // 기존 이미지 데이터에 새로운 데이터를 추가 일케하면 계속생기네?
+      console.log(data.data.totalElements); // 이걸 안넘게하면되나?
+      console.log('데이터 개수:', articleData);
+      return data;
+    },
+  });
+
+  useEffect(() => {
+    if (!isPending && target.current) {
+      const observer = new IntersectionObserver(
+        async (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              setEndIndex((prev) =>
+                Math.min(prev + 10, data.data.totalElements),
+              );
+              console.log('관찰 대상 요소가 화면에 나타남');
+              // 관련 작업 수행
+            } else {
+              return () => {
+                if (target.current) observer.unobserve(target.current);
+              };
             }
-      }
-      login();
-  },[])
+          });
+        },
+        { threshold: 0 },
+      );
+
+      observer.observe(target.current);
+
+      // 컴포넌트가 언마운트될 때 옵저버를 정리하기 위해 cleanup 함수를 반환
+    }
+  }, [isPending]); // 빈 배열을 의존성 배열로 전달하여 한 번만 실행되도록 설정
 
   return (
-     <div> 123456 </div> 
+    <div>
+      <div className="container">
+        {articleData.slice(startIndex, endIndex).map((article) => (
+          <div key={article.id} className="box">
+            <img
+              src={`${apiBaseUrl}/${article.imgFilePath}/${article.imgFileName}`}
+              alt={``}
+            />
+          </div>
+        ))}
+      </div>
+      <div ref={target}></div>
+    </div>
   );
 };
-export default Home
+export default Home;

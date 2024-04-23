8df754622f2e2cbb537c96019e242dc7d862e8ad
thseo1758 <thseo1758@gmail.com>
2024-04-24 01:12:03 +0900

    fix:스크롤로 이미지 추가시 이미지가 출력되지 않던 문제fix

--- src/api/reactQuery/imageDataQuery.ts
+++ src/api/reactQuery/imageDataQuery.ts
@@ -12,17 +12,17 @@ type ArticleData = {
 const apiUrl = process.env.REACT_APP_CORE_API_BASE_URL;
 export function useImageData() {
   const [articleData, setArticleData] = useState<ArticleData[]>([]);
-  const { isPending, error, data } = useQuery({
+  const { isPending, error, data, refetch } = useQuery({
     queryKey: ['imageData'],
     queryFn: async () => {
       const res = await fetch(`${apiUrl}/api/article`); // API에서 이미지 데이터를 가져옴
       const data = await res.json(); // 응답 데이터를 JSON 형식으로 변환
-      setArticleData(data.data.content);
+      setArticleData((c) => c.concat(data.data.content));
       // 데이터를 반환할 필요가 있는지 여부에 따라 적절하게 수정
 
       return data;
     },
   });
 
-  return { isPending, error, data, articleData };
+  return { isPending, error, data, articleData, refetch };
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useImageData } from '../api/reactQuery/imageDataQuery';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import Masonry from 'react-masonry-css';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -10,43 +11,46 @@ const Home = () => {
   const [totalPages, setTotalPages] = useState(0);
   const [startIndex, setStartIndex] = useState(0); // 표시할 이미지의 시작 인덱스
   const [endIndex, setEndIndex] = useState(10); // 표시할 이미지의 마지막 인덱스
+  const [morePhoto, setMorePhto] = useState(true);
   const apiBaseUrl = process.env.REACT_APP_CORE_IMAGE_BASE_URL;
 
-  const { isPending, data, articleData } = useImageData();
+  const { isPending, data, articleData, refetch } = useImageData();
 
-  useEffect(() => {
-    if (!isPending && target.current) {
-      const observer = new IntersectionObserver(
-        async (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              setEndIndex((prev) =>
-                Math.min(prev + 10, data.data.totalElements),
-              );
-              console.log('관찰 대상 요소가 화면에 나타남');
-              // 관련 작업 수행
-            } else {
-              return () => {
-                if (target.current) observer.unobserve(target.current);
-              };
-            }
-          });
-        },
-        { threshold: 0 },
+  const breackpoindtColumns = {
+    default: 3,
+    768: 2,
+    480: 1,
+  };
+  const fetchData = () => {
+    setTimeout(() => {
+      setEndIndex(
+        (prev) => Math.min(prev + 10, data.data.totalElements), //보일시에 endindex를 늘려줘  마손니가 추가돼야함. 마지막걸
       );
+      refetch();
 
-      observer.observe(target.current);
-
-      // 컴포넌트가 언마운트될 때 옵저버를 정리하기 위해 cleanup 함수를 반환
-    }
-  }, [isPending]); // 빈 배열을 의존성 배열로 전달하여 한 번만 실행되도록 설정
-
+      if (endIndex === data.data.totalElements) return setMorePhto(false);
+      setMorePhto(true);
+    }, 500);
+  };
+  console.log(endIndex);
   return (
     <div>
-      <ResponsiveMasonry columnsCountBreakPoints={{ 480: 1, 768: 2, 1200: 3 }}>
-        <Masonry gutter="20px">
-          {articleData.slice(startIndex, endIndex).map((article) => (
-            <div key={article.id} className="box">
+      <InfiniteScroll
+        dataLength={articleData.slice(startIndex, endIndex).length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={morePhoto}
+        loader={<h4 className="flex justify-center">Loading...</h4>}
+        endMessage={
+          <h4 className="flex justify-center">마지막 이미지입니다.</h4>
+        }
+      >
+        <Masonry
+          breakpointCols={breackpoindtColumns}
+          className="my-masonry-grid flex gap-3"
+          columnClassName="my-masonry-grid_column"
+        >
+          {articleData.map((article) => (
+            <div key={article.id} className="box mb-3">
               <img
                 className="rounded-2xl"
                 src={`${apiBaseUrl}/${article.imgFilePath}/${article.imgFileName}`}
@@ -54,9 +58,9 @@ const Home = () => {
               />
             </div>
           ))}
-          <div ref={target}></div>
         </Masonry>
-      </ResponsiveMasonry>
+        <div ref={target}></div>
+      </InfiniteScroll>
     </div>
   );
 };

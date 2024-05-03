0199593b08814ae440da5086e7c610f7fa18a02d
SK\ssssk <ssssksss@naver.com>
2024-05-03 10:01:30 +0900

    feat: 작업 안한 퍼블리싱

---
+++ src/pages/mypage/MyBuyList.tsx
@@ -0,0 +1,11 @@
+import MypageLayout from "./MypageLayout";
+
+const MyBuyList = () => {
+  return (
+    <div className="flex h-full w-full flex-col">
+      <MypageLayout />
+      <div className="flex h-full w-full justify-center"></div>
+    </div>
+  );
+};
+export default MyBuyList
---
+++ src/pages/mypage/MyOrderDetail.tsx
@@ -0,0 +1,6 @@
+const MyOrderDetail = () => {
+  return (
+     <div> 123 </div> 
+  );
+};
+export default MyOrderDetail
